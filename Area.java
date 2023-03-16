d34534d84e0bf72d355814868351e95544f13288
ashwinidhaybhai <dhaybhaiashwini@gmail.com>
2023-03-16 13:02:56 +0530

    This program cacluate area of circle

---
+++ Area.java
@@ -0,0 +1,21 @@
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float radius = 6.5f;
+	    final float pie = 3.14f;
+	    double area = pie *radius*radius;
+	   // System.out.print("Area of circle is:"+ area);
+
+		
+	    System.out.print("Area of circle is:"+ String.format("%.2f ",area));
+	    // this statement is use when you want to show upto 2 decimal values
+	    
+		
+		
+	
+
+	}
+
+}
